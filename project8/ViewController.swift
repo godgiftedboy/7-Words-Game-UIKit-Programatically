e3ecb2bfd8c18572b5e91bc5e2f30ad8d7ea1a54
godgiftedboy <rajprajapati12321@gmail.com>
2025-04-09 11:19:02 +0545

    chore: added constraints to the UITextField currentAnswer

--- project8/ViewController.swift
+++ project8/ViewController.swift
@@ -69,7 +69,13 @@ class ViewController: UIViewController {
             answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4,constant: -100),
             
             //equal height of both labels
-            answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor)
+            answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor),
+            
+            
+            currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor,multiplier: 0.5),
+            currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor,constant: 20),
+            
 
             // more constraints to be added here!
         ])
